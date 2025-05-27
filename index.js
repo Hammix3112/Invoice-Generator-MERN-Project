e783479bf67007a1b137d0327ee76bc279dc1e55
Hammix3112 <hamaadjanjua64@gmail.com>
2025-05-27 12:18:20 +0500

    again

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "dist")));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
